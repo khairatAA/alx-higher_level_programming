bd24a5f6f8b6dc36047d3654a5896d960d9b7007
khairatAA <khairatadesina01@gmail.com>
2023-08-11 22:51:42 +0100

    task 4 & 9 completed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3.8
-import marshal
+import hidden_4
 
 if __name__ == "__main__":
-    with open("/mnt/c/Users/HP/Downloads/hidden_4.pyc", "rb") as file:
-        code = marshal.load(file)
-    names = code.co_names
-    sorted_names = sorted(name for name in names if not name.startswith("__"))
-    for name in sorted_names:
-        print("{}".format(name))
+    hidden_names = dir(hidden_4)
+    for name in hidden_names:
+        if name[:2] != "__":
+            print("{}".format(name))

eefa60bf7fb6c7c9d072509ecad1439285b7d192
khairatAA <khairatadesina01@gmail.com>
2023-08-12 15:22:57 +0100

    task 0 -- updated

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in range(0, len(my_list)):
-        value = int(my_list[i])
-        print("{}".format(value))
+        print("{:d}".format(i))

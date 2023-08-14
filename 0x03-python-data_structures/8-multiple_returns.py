7faf7ed3614d926d670bc80c17843dd53c6f90de
khairatAA <khairatadesina01@gmail.com>
2023-08-14 14:40:33 +0100

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        sentence = "None"
+        tuple_result = (0, sentence)
+        return tuple_result
+    else:
+        count = len(sentence)
+        tuple_result = (count, sentence[0])
+        return tuple_result

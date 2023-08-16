b72886be2ca8ed632071200bc5d327ac3f7b1ace
khairatAA <khairatadesina01@gmail.com>
2023-08-16 23:34:04 +0100

    task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     num = 0
-    for i in range(len(a_dictionary)):
-        num = num + i
+    for i in a_dictionary:
+        num = num + 1
+        if isinstance(a_dictionary[i], dict):
+            num = num + number_keys(a_dictionary[i])
     return num

11ec34889afe7a12be5a6dd34250d4471014dce2
khairatAA <khairatadesina01@gmail.com>
2023-08-17 00:32:04 +0100

    task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_key = sorted(a_dictionary.keys())
+    for i in sorted_key:
+        value = a_dictionary[i]
+        print("{}: {}".format(i, value))

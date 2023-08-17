b22b210d75afe96d9db7655c36857beb85eef2a9
khairatAA <khairatadesina01@gmail.com>
2023-08-17 06:19:56 +0100

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    for i in a_dictionary:
+        if i == key:
+            a_dictionary[i] = value
+        else:
+            i = key
+            a_dictionary[i] = value
+        return a_dictionary

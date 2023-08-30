109dd46b5453b544166d0a559a77c60248a05bf8
khairatAA <khairatadesina01@gmail.com>
2023-08-30 04:42:39 +0100

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""3-square.py"""
+"""4-square.py"""
 
 
 class Square:
@@ -10,11 +10,25 @@ class Square:
         Args:
             size (int): this is a private instance attribute.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+
+    @property
+    def size(self):
+        """This is the getter property used to retrieve size.
+
+        Args:
+            value (int): the variable in the setter property the holds the new
+            size value.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """This calculates the area of the Square.

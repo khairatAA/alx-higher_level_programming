609da6e67487eb246ff076a1410bc344d53441c1
khairatAA <khairatadesina01@gmail.com>
2023-08-30 23:59:57 +0100

    task 9 correction

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
-result = 0
-for i in range(1, 3):
-    try:
-        if i > a:
-            raise Exception("Too far")
-            result += a ** i / i
-    except Exception:
-        pass
-    else:
-        result += b
-    finally:
-        return result
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+                result += a ** b / i
+        except Exception:
+            pass
+        else:
+            result += a + b
+        finally:
+            return result
---
+++ 0x05-python-exceptions/test-102.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+result = magic_calculation(1, 2)
+print(result)

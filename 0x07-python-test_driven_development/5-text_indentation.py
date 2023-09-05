b56b93db8a4f5981e7033ac167c9f64fea4cf062
khairatAA <khairatadesina01@gmail.com>
2023-09-05 12:40:50 +0100

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python3
 """
+prints a text with 2 new lines after each
+of these characters: ., ? and :.
 5-text_indentation.py
 """
 
 
 def text_indentation(text):
+    """prints a text with 2 new lines after
+    each of these characters: ., ? and :.
+
+    Args:
+        character(list) : containing the characters.
+        count(int): keeps track of the number of loops
+        lines(list): list containing the splitted string
+
+    Raises:
+        TypeError: text must be a string
+        ValueError: input must not be an empty string
+    """
     if type(text) is not str or text is None:
         raise TypeError("text must be a string")
+    elif not text.strip():
+        raise ValueError("input must not be an empty string")
+    else:
+        characters = ['.', '?', ':']
 
-    characters = ['.', '?', ':']
+        for char in characters:
+                text = text.replace(char + ' ', char + '\n')
 
-    for char in characters:
-            text = text.replace(char + ' ', char + '\n\n')
+        lines = text.split('\n')
+        count = 0
 
-    lines = text.split('\n')
-
-    for line in lines:
-        stripped_line = line.strip()
-        if stripped_line:
-            print(stripped_line)
+        for line in lines:
+            stripped_line = line.strip()
+            count += 1
+            if stripped_line and count < len(lines):
+                print(stripped_line)
+                print()
+            else:
+                print(stripped_line, end="")

31ad24bbf029cde82d31c0a42ab22c0a919c945d
khairatAA <khairatadesina01@gmail.com>
2023-09-06 11:47:13 +0100

    task 4 and 5 correction...

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -8,23 +8,12 @@ def max_integer(input_list=[]):
     """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
     """
-    if not isinstance(input_list, list):
-        raise TypeError("Input must be a list of integers")
-
-    if len(input_list) == 0:
+    if len(list) == 0:
         return None
-
-    for i in input_list:
-        if type(i) is not int:
-            raise TypeError("Input must be a list of integers")
-
-        if i is True or i is False:
-            raise TypeError("Input must be a list of integers")
-
-    result = input_list[0]
-
-    for i in input_list:
-        if i > result:
-            result = i
-
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
     return result

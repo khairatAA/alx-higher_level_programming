bb31af57e9925f811d53ffe41236d9954b7030c0
khairatAA <khairatadesina01@gmail.com>
2023-09-11 17:11:30 +0100

    task 1 correction

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,20 @@
 class MyList(list):
     """a class MyList that inherits list"""
     def print_sorted(self):
-        """prints the list, but sorted (ascending sort)"""
-        self.sort()
-        print(self)
+        """prints the list, but sorted (ascending sort)
+        Raises:
+            ValueError: Input is not a list.
+            ValueError: Input must be a list of numbers.
+        """
+
+        if not isinstance(self, list):
+            raise ValueError("Input is not a list.")
+
+        if not all(isinstance(x, int) for x in self):
+            raise ValueError("Input must be a list of numbers.")
+
+        if len(self) == 0:
+            raise ValueError("Input must be a list of numbers.")
+        else:
+            self = sorted(self)
+            print(self)

f1001cf66c6ae473c510ed9f99fd145dc6713781
khairatAA <khairatadesina01@gmail.com>
2023-09-13 16:45:41 +0100

    task 4 more corrections...

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,45 +1,6 @@
 #!/usr/bin/python3
 """8-rectangle"""
-
-
-class BaseGeometry:
-    """This is the  BaseGeometry class"""
-
-    def area(self):
-        """Public instance method -
-        that raises an Exception with the message
-        area() is not implemented
-        Raises:
-            Exception: area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method.
-        validates value.
-        Raises:
-            TypeError: name must be a string
-            TypeError: name must not be an empty string
-            TypeError: "{} must be an integer".format(name)"
-            VlaueError: "{} must be greater than 0".format(name)"
-        """
-        if not isinstance(name, str):
-            raise TypeError("name must be a string")
-
-        if name is None or name is bool:
-            raise TypeError("name must be a string")
-
-        if len(name) == 0:
-            raise TypeError("name must not be an empty string")
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-
-        if value is None or value is bool:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

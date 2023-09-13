567986d2bcc5504f2c1eff34f93d2ccda47a6832
khairatAA <khairatadesina01@gmail.com>
2023-09-13 15:59:45 +0100

    task 4 correction

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,7 +8,7 @@ def inherits_from(obj, a_class):
     class ; otherwise False
     """
 
-    if isinstance(a_class) is obj:
+    if isinstance(obj, a_class) and (type(obj) is not a_class):
         return True
-
-    return False
+    else:
+        return False

313e3568740fbb621d394112c1795b189ee016ee
Khairat Adesina <khairatadesina01@gmail.com>
2023-10-19 12:26:50 +0100

    lists all the cities of California that can be found in the database hbtn_0d_usa

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,10 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 
-SELECT *
-FROM states
-WHERE name = "California"
-ORDER BY name ASC;
+SELECTcities.id,  cities.name
+FROM cities
+WHERE cities.id = (
+	SELECT state.id
+	FROM state
+	WHERE states.name = 'California'
+	)
+ORDER BY cities.id ASC;

a4fd5f5adaf539205692a1502e56cf13f14d1ab6
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-14 19:32:35 +0100

    updated task 1 -> 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// class Rectangle that defines a rectangle
+
+class Rectangle {
+  width;
+  height;
+
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;

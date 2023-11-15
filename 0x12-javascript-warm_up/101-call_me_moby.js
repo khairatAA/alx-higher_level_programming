575d7028348d564d06ea063b4cc7ecdc4feb11b4
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-15 18:12:45 +0100

    add:: solutions to task 15

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// executes x times a function
+
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+module.exports.callMeMoby = callMeMoby;

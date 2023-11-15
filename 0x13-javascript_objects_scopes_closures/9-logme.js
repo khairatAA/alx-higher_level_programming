f635beded883ec41d33698b64222f39f96ec2d02
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-15 14:05:10 +0100

    completed task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// prints the number of arguments already printed and the new argument value.
+
+let count = 0;
+
+exports.logMe = function (item) {
+  const info = (count + ': ' + item);
+  count += 1;
+
+  console.log(info);
+};

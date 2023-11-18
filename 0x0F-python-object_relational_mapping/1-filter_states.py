f77e76352f8aa6f47a8f3292f974797af477dee4
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-18 11:41:42 +0100

    add: completed 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''The 1-filter_states module'''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    '''lists all states with a name starting with N 
+    (upper N) from the database hbtn_0e_0_usa'''
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        port=3306,
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    state_col = cur.fetchall()
+
+    for row in state_col:
+        print("{}".format(row))
+
+    cur.close()
+    db.close()

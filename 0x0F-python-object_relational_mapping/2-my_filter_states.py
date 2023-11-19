c0e91fa52e99f17fd2149d13f984184b77031f06
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-19 12:39:31 +0100

    add: 2-my_filter_states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''2-my_filter_states module'''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    ''' takes in an argument and displays all values in
+    the states table of hbtn_0e_0_usa where name matches
+    the argument.'''
+    connection = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        port=3306,
+        db=sys.argv[3]
+    )
+
+    cur = connection.cursor()
+
+    query = (
+        "SELECT * FROM states WHERE name "
+        "LIKE '{}' ORDER BY id ASC".format('%' + sys.argv[4] + '%')
+    )
+
+    cur.execute(query)
+    state_col = cur.fetchall()
+
+    for row in state_col:
+        print("{}".format(row))
+
+    cur.close()
+    connection.close()

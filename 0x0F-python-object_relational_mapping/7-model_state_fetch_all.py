5d2b354b4ab264c0386f336d5fa95e96ab819af2
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-27 17:59:44 +0100

    add: All states via SQLAlchemy(task 7)

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""7-model_state_fetch_all module"""
+from model_state import Base, State
+from sqlalchemy import create_engine, select
+import sys
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        user, passwd, db), pool_pre_ping=True)
+
+    conn = engine.connect()
+
+    Base.metadata.reflect(engine)
+    states = Base.metadata.tables['states']
+
+    query = select(states).order_by(states.c.id.asc())
+
+    output = conn.execute(query)
+
+    result = output.fetchall()
+
+    for i in result:
+        print("{}: {}".format(i.id, i.name))
+
+    Base.metadata.create_all(engine)

54f13b15df6801ac86a678e99559a6d684dc58c5
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-30 19:47:30 +0100

    fix: the filter

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -s -i -X OPTIONS $1 | grep -i 'Allow:' | tr -d '\r'
+curl -s -i -X OPTIONS $1 | grep 'Allow:' | cut -d " " -f 2-

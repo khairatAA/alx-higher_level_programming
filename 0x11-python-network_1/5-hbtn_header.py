ebaa84a9595dba617513987aebd447cf5f317a44
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-01 20:59:59 +0100

    add: Response header value #1 (task 5)

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""5-hbtn_header module"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """
+    Python script that takes in a URL,
+    sends a request to the URL and displays
+    the value of the variable X-Request-Id
+    in the response header
+    """
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    X_Request_Id = response.headers['X-Request-Id']
+    print("{}".format(X_Request_Id))

c65dc40ec4b598ff207373c165ff05c7bf37d976
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-02 17:14:36 +0100

    add: My GitHub! (task 9)

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""10-my_github"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """
+    Takes your GitHub credentials (username and password)
+    and uses the GitHub API to display your id
+    """
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    auth = (username, password)
+    response = requests.get('https://api.github.com/user', auth=auth)
+
+    if response.status_code == 200:
+        json = response.json()
+        print(json["id"])

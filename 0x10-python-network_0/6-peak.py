c99ff8a5b154076853f964d0c17fc972db7e55f1
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-03 02:05:20 +0100

    add: Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,37 @@
+#!/bin/usr/python3
+"""6-peak module"""
+
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers
+    Args:
+        list_of_integers: a list containg integers
+    """
+
+    if not list_of_integers:
+        return None
+
+    return binary_search(list_of_integers, 0, len(list_of_integers) - 1)
+
+
+def binary_search(list_of_integers, left, right):
+    """
+    Binary Search for peak
+    Args:
+        list_of_integers: a list containg integers
+        left: left index
+        right: right index
+    """
+    if left == right:
+        return list_of_integers[left]
+
+    mid = (left + right) // 2
+
+    if mid == 0 and list_of_integers[mid] > list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return binary_search(list_of_integers, mid + 1, right)
+    else:
+        return binary_search(list_of_integers, left, mid)

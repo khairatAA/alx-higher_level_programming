1798cb03653aed0cc9f9bce859feb54d38b3dc9b
kKhairat Adesina <khairatadesina01@gmail.com>
2024-01-05 03:50:02 +0100

    add: script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// 1-writeme
+
+// A script that writes a string to a file.
+const fs = require('node:fs');
+const processArgv = require('process').argv;
+
+fs.writeFile(processArgv[2], processArgv[3], err => {
+  if (err) {
+    console.error(err);
+  }
+});
